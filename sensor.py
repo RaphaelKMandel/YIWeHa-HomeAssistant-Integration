3833d06a852276274fbe821580927e14795295e4
Raphael Mandel <raphael.k.mandel@gmail.com>
2025-04-15 10:21:11 -0400

    updated CandleLighting and Havdalah Sensors to only update at midnight, and filter Events from midnight instead of now

--- sensor.py
+++ sensor.py
@@ -42,24 +42,35 @@ class NextCandleLightingSensor(CoordinatorEntity, SensorEntity):
         super().__init__(coordinator)
         self._attr_name = "Next Candle Lighting"
         self._attr_unique_id = f"{DOMAIN}_next_candle_lighting"
+        self._next_time = None
+
+    def _handle_coordinator_update(self) -> None:
+        """Handle updated data from the coordinator."""
+        if not self.coordinator.data:
+            self._next_time = None
+            return
+            
+        candle_lighting_times = self.coordinator.data[0]  # First element of tuple
+        if not candle_lighting_times:
+            self._next_time = None
+            return
+            
+        # Get midnight of current day
+        now = datetime.now()
+        midnight = datetime(now.year, now.month, now.day)
+        future_times = [t for t in candle_lighting_times if t.datetime > midnight]
+        
+        if not future_times:
+            self._next_time = None
+            return
+            
+        self._next_time = min(future_times).datetime
+        self.async_write_ha_state()
 
     @property
     def native_value(self):
         """Return the next candle lighting time."""
-        if not self.coordinator.data:
-            return None
-            
-        candle_lighting_times = self.coordinator.data[0]  # First element of tuple
-        if not candle_lighting_times:
-            return None
-            
-        now = datetime.now()
-        future_times = [t for t in candle_lighting_times if t.datetime > now]
-        
-        if not future_times:
-            return None
-            
-        return min(future_times).datetime
+        return self._next_time
 
 class NextHavdalahSensor(CoordinatorEntity, SensorEntity):
     """Sensor for next havdalah time."""
@@ -69,24 +80,35 @@ class NextHavdalahSensor(CoordinatorEntity, SensorEntity):
         super().__init__(coordinator)
         self._attr_name = "Next Havdalah"
         self._attr_unique_id = f"{DOMAIN}_next_havdalah"
+        self._next_time = None
+
+    def _handle_coordinator_update(self) -> None:
+        """Handle updated data from the coordinator."""
+        if not self.coordinator.data:
+            self._next_time = None
+            return
+            
+        havdalah_times = self.coordinator.data[1]  # Second element of tuple
+        if not havdalah_times:
+            self._next_time = None
+            return
+            
+        # Get midnight of current day
+        now = datetime.now()
+        midnight = datetime(now.year, now.month, now.day)
+        future_times = [t for t in havdalah_times if t.datetime > midnight]
+        
+        if not future_times:
+            self._next_time = None
+            return
+            
+        self._next_time = min(future_times).datetime
+        self.async_write_ha_state()
 
     @property
     def native_value(self):
         """Return the next havdalah time."""
-        if not self.coordinator.data:
-            return None
-            
-        havdalah_times = self.coordinator.data[1]  # Second element of tuple
-        if not havdalah_times:
-            return None
-            
-        now = datetime.now()
-        future_times = [t for t in havdalah_times if t.datetime > now]
-        
-        if not future_times:
-            return None
-            
-        return min(future_times).datetime
+        return self._next_time
 
 class IssurMelachaSensor(CoordinatorEntity, BinarySensorEntity):
     """Binary sensor for Issur Melacha status."""
@@ -98,49 +120,48 @@ class IssurMelachaSensor(CoordinatorEntity, BinarySensorEntity):
         self._attr_unique_id = f"{DOMAIN}_issur_melacha"
         self._attr_device_class = "running"
         self._state = False
-        self._next_candle_lighting = None
-        self._next_havdalah = None
 
-    def _update_next_times(self) -> None:
-        """Update the next candle lighting and havdalah times."""
-        now = datetime.now()
-        
-        # Get next candle lighting
+    def _handle_coordinator_update(self) -> None:
+        """Handle updated data from the coordinator."""
+        if not self.coordinator.data:
+            self._state = False
+            return
+
         candle_lighting_times = self.coordinator.data[0]
-        future_candle_lighting = [t for t in candle_lighting_times if t.datetime > now]
-        self._next_candle_lighting = min(future_candle_lighting).datetime if future_candle_lighting else None
-
-        # Get next havdalah
         havdalah_times = self.coordinator.data[1]
-        future_havdalah = [t for t in havdalah_times if t.datetime > now]
-        self._next_havdalah = min(future_havdalah).datetime if future_havdalah else None
+        
+        if not candle_lighting_times or not havdalah_times:
+            self._state = False
+            return
+
+        # Get midnight of current day
+        now = datetime.now()
+        midnight = datetime(now.year, now.month, now.day)
+        
+        # Get all future events after midnight
+        future_candle_lighting = [t for t in candle_lighting_times if t.datetime > midnight]
+        future_havdalah = [t for t in havdalah_times if t.datetime > midnight]
+        
+        if not future_candle_lighting and not future_havdalah:
+            self._state = False
+            return
+            
+        # Find the next event (either candle lighting or havdalah)
+        next_candle_lighting = min(future_candle_lighting).datetime if future_candle_lighting else None
+        next_havdalah = min(future_havdalah).datetime if future_havdalah else None
+        
+        # Determine state based on which event comes next
+        if next_candle_lighting and next_havdalah:
+            # If havdalah comes before candle lighting, we're in Issur Melacha
+            self._state = next_havdalah < next_candle_lighting
+        else:
+            # If we only have one type of event, use that to determine state
+            self._state = bool(next_havdalah)  # True if next event is havdalah, False if candle lighting
+            
+        self.async_write_ha_state()
 
     @property
     def is_on(self) -> bool:
         """Return True if currently in Issur Melacha period."""
-        if not self.coordinator.data:
-            return False
-
-        now = datetime.now()
-
-        # If we don't have next times stored, or we're not in Issur Melacha,
-        # update the next times
-        if not self._state or (not self._next_candle_lighting and not self._next_havdalah):
-            self._update_next_times()
-
-        # If we're waiting for candle lighting
-        if not self._state and self._next_candle_lighting:
-            if now >= self._next_candle_lighting:
-                self._state = True
-                # Clear the next candle lighting since we've passed it
-                self._next_candle_lighting = None
-
-        # If we're in Issur Melacha and waiting for havdalah
-        elif self._state and self._next_havdalah:
-            if now >= self._next_havdalah:
-                self._state = False
-                # Update times for the next cycle
-                self._update_next_times()
-
         return self._state
 
