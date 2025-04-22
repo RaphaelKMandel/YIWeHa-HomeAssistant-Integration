acf0a8dfef376c4b6f88f74c7667fa922703170e
Raphael Mandel <raphael.k.mandel@gmail.com>
2025-04-22 10:02:55 -0400

    call async setup entry refresh

--- __init__.py
+++ __init__.py
@@ -29,6 +29,7 @@ def get_next_midnight():
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     coordinator = MidnightCoordinator(hass)
+    await coordinator.async_config_entry_first_refresh()
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
